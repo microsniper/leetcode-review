367bef562ae8204065d2074f5f0f95b6601598ca
zp <sniper_zp@163.com>
2024-07-01 15:05:06 +0800

    add topic

---
+++ src/main/java/com/sniper/leetcodereview/solution/leetcode/topic_3/Solution.java
@@ -0,0 +1,39 @@
+package com.sniper.leetcodereview.solution.leetcode.topic_3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description
+ * @Author sniper
+ * @Date 2024/6/30 7:12 PM
+ **/
+public class Solution {
+    // 滑动窗口
+    public int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+
+        if (len == 0){
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+        while(left <= right && right < len){
+            if (!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                right++;
+                max = Math.max(max,set.size());
+            }else{
+                max = Math.max(max,set.size());
+                while(left <= right && set.contains(s.charAt(right))){
+                    set.remove(s.charAt(left));
+                    left++;
+                }
+            }
+
+        }
+        return max;
+    }
+}
